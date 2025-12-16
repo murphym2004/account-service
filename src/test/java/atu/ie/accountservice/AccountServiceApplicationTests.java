622da6b1ecd29182579e09b5d1d092fab54e9c38
matthew <g00430349@atu.ie>
2025-12-16 18:59:24 +0000

    AccountService initialization and seperation from main service

---
+++ src/test/java/atu/ie/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

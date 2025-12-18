7adeaf77329804915efe64a4fa5ebfb1fdef128f
matthew <g00430349@atu.ie>
2025-12-18 14:56:49 +0000

    AccountService initialization and seperation from main service

--- src/main/java/atu/ie/accountservice/controller/AccountController.java
+++ src/main/java/atu/ie/accountservice/controller/AccountController.java
@@ -15,11 +15,13 @@ public class AccountController {
     private final AccountService accountService;
 
     public AccountController(AccountService accountService) {
+
         this.accountService = accountService;
     }
 
     @GetMapping
     public List<Account> getAccountlist(){
+
         return accountService.findAll();
     }
 
